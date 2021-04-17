a35cc03268d564e8a11b74e8077f0ea0d5ec9601
Raymond M <raymondmei.rm@gmail.com>
2021-04-17 13:16:06 -0400

    Remove board duplication to make it faster

--- BotMoves.py
+++ BotMoves.py
@@ -3,46 +3,112 @@ import copy
 def printBoard(board):
     print(*board,sep='\n')
 
-def checkWin(board):
+def checkWin(board, boardSize, winCondition): 
     # a tuple - (if win exists, returns 1 if player1 wins 0 if player2/CPU wins)
     # check rows and cols
-    for i in range(3):
-        if(board[i][0] != 0 and board[i][0] == board[i][1] and board[i][1] == board[i][2]):
-            return (1, board[i][0] == 1)
-        if(board[0][i] != 0 and board[0][i] == board[1][i] and board[1][i] == board[2][i]):
-            return (1, board[0][i] == 1)
-
+    for i in range(boardSize):
+        streak = 0
+        if(board[i][0] != 0):
+            streak = 1
+        for m in range(1, boardSize):
+            if(board[i][m] == 0):
+                streak = 0
+            elif(board[i][m] != board[i][m - 1]):
+                streak = 1
+            else:
+                streak += 1
+                if(streak == winCondition):
+                    return (1, board[i][m] == 1)
+        streak = 0
+        if(board[0][i] != 0):
+            streak = 1
+        for n in range(1, boardSize):
+            if(board[n][i] == 0):
+                streak = 0
+            elif(board[n][i] != board[n - 1][i]):
+                streak = 1
+            else:
+                streak += 1
+                if(streak == winCondition):
+                    return (1, board[n][i] == 1)
     # check diagonals
-    if(board[0][0] != 0 and board[0][0] == board[1][1] and board[1][1] == board[2][2]):
-        return (1, board[1][1] == 1)
-    if(board[0][2] != 0 and board[0][2] == board[1][1] and board[1][1] == board[2][0]):
-        return (1, board[1][1] == 1)
-
+    for i in range(boardSize):
+        streak = 0
+        if(board[i][0] != 0):
+            streak = 1
+        for m in range(1, boardSize - i):
+            if(board[i + m][m] == 0):
+                streak = 0
+            elif(board[i + m][m] != board[i + m - 1][m - 1]):
+                streak = 1
+            else:
+                streak += 1
+                if(streak == winCondition):
+                    return (1, board[i + m][m] == 1)
+    for i in range(boardSize):
+        streak = 0
+        if(board[0][i] != 0):
+            streak = 1
+        for n in range(1, boardSize - i):
+            if(board[n][i + n] == 0):
+                streak = 0
+            elif(board[n][i + n] != board[n - 1][i + n - 1]):
+                streak = 1
+            else:
+                streak += 1
+                if(streak == winCondition):
+                    return (1, board[n][i + n] == 1)
+    for i in range(boardSize):
+        streak = 0
+        if(board[i][0] != 0):
+            streak = 1
+        for m in range(1, i + 1):
+            if(board[i - m][m] == 0):
+                streak = 0
+            elif(board[i - m][m] != board[i - m + 1][m - 1]):
+                streak = 1
+            else:
+                streak += 1
+                if(streak == winCondition):
+                    return (1, board[i - m][m] == 1)
+    for i in range(boardSize):
+        streak = 0
+        if(board[boardSize - 1][i] != 0):
+            streak = 1
+        for m in range(2, boardSize - i + 1):
+            if(board[boardSize - m][i + m - 1] == 0):
+                streak = 0
+            elif(board[boardSize - m][i + m - 1] != board[boardSize - m + 1][i + m - 2]):
+                streak = 1
+            else:
+                streak += 1
+                if(streak == winCondition):
+                    return (1, board[boardSize - m][i + m - 1] == 1)
     return (0, 0)
 
-def botMove(board, depth):
+def botMove(board, depth, boardSize, winCondition):
     bestScore = float('inf') # bot wants to minimize score, player wants to maximize
     row, col = -1, -1
 
-    gameWon, player = checkWin(board) # check if game is already won first
+    gameWon, player = checkWin(board, boardSize, winCondition) # check if game is already won first
     if(gameWon):
         return row, col
 
-    for i in range(3):
-        for j in range(3):
+    for i in range(boardSize):
+        for j in range(boardSize):
             if(board[i][j] == 0):                 # visit each available cell
-                temp = copy.deepcopy(board)     # duplicate original board
-                temp[i][j] = 2                    # simulate the bot moving to that cell
-                score = minimax(temp, depth-1, False, float('-inf'), float('inf'))
+                board[i][j] = 2
+                score = minimax(board, depth-1, boardSize, winCondition, False, float('-inf'), float('inf'))
+                board[i][j] = 0
                 if(score < bestScore):
                     bestScore = score
                     row, col = i, j
 
     return row, col
 
-def minimax(board, depth, bot, alpha, beta):
+def minimax(board, depth, boardSize, winCondition, bot, alpha, beta):
 
-    gameWon, player = checkWin(board)
+    gameWon, player = checkWin(board, boardSize, winCondition)
     if(gameWon): 
         if(player == 1): return 1 * (depth+1)    # positive score if player wins, negative if bot wins
         else: return -1 * (depth+1)                # multiply by depth to prioritize winning in the fewest moves possible (+1 to avoid multiplying by 0)
@@ -52,23 +118,23 @@ def minimax(board, depth, bot, alpha, beta):
 
     if(bot): # bot's turn (minimizing)
         bestScore = float('inf')
-        for i in range(3):
-            for j in range(3):
+        for i in range(boardSize):
+            for j in range(boardSize):
                 if(board[i][j] == 0):
-                    temp = copy.deepcopy(board)
-                    temp[i][j] = 2
-                    bestScore = min(bestScore, minimax(temp, depth-1, False, alpha, beta))
+                    board[i][j] = 2
+                    bestScore = min(bestScore, minimax(board, depth-1, boardSize, winCondition, False, alpha, beta))
+                    board[i][j] = 0
                     beta = min(beta, bestScore)
                     if(beta <= alpha):
                         break
     else: # player's turn (maximizing)
         bestScore = float('-inf')
-        for i in range(3):
-            for j in range(3):
+        for i in range(boardSize):
+            for j in range(boardSize):
                 if(board[i][j] == 0):
-                    temp = copy.deepcopy(board)
-                    temp[i][j] = 1
-                    bestScore = max(bestScore, minimax(temp, depth-1, True, alpha, beta))
+                    board[i][j] = 2
+                    bestScore = max(bestScore, minimax(board, depth-1, boardSize, winCondition, True, alpha, beta))
+                    board[i][j] = 0
                     alpha = max(alpha, bestScore)
                     if(beta <= alpha):
                         break
